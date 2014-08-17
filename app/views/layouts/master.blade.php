820d8e5c897b910e6164fdd632042804a576ad3d
HexagonStorms <josueplazamusic@gmail.com>
2014-08-17 15:44:32 -0500

    Deleting unnecessary filter code

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -97,64 +97,7 @@
 
         <!-- layout-->
         <div id="layout">
-            <!-- Login Client -->
-            <div class="jBar">
-              <div class="container">            
-                  <div class="row">                     
-                      <form>            
-                          <div class="col-md-2">
-                              <h1>Login Area</h1> 
-                          </div>
-                          <div class="col-md-4"> 
-                              <input type="email" placeholder="Your Email" required>
-                          </div>
-                          <div class="col-md-3">
-                              <input type="password" placeholder="Your Password" required>                          
-                          </div>
-                          <div class="col-md-3">
-                              <input type="submit" class="botton" value="Sign in">
-                              <span>Our</span>
-                              <input type="submit" class="botton" value="Register">
-                          </div>
-                      </form>              
-                      <p class="jTrigger downarrow">Close Login</p>
-                  </div>
-              </div>
-            </div>
-            <span class="jRibbon jTrigger up">Login</span>
-            <div class="line"></div>
-            <!-- End Login Client -->
 
-            <!-- Info Head -->
-            <section class="info_head">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <ul>  
-                                <li><i class="fa fa-headphones"></i><a href="#">01800034567</a></li>
-                                <li><i class="fa fa-comment"></i><a href="#">Live chat</a></li>
-                                <li>
-                                    <ul>
-                                      <li class="dropdown">
-                                        <i class="fa fa-globe"></i> 
-                                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                                            Language<i class="fa fa-angle-down"></i>
-                                        </a>
-                                         <ul class="dropdown-menu">  
-                                             <li><a href="#"><img src="img/language/spanish.png" alt="">Spanish</a></li>
-                                             <li><a href="#"><img src="img/language/english.png" alt="">English</a></li>
-                                             <li><a href="#"><img src="img/language/frances.png" alt="">Frances</a></li>
-                                             <li><a href="#"><img src="img/language/portugues.png" alt="">Portuguese</a></li>
-                                        </ul>
-                                      </li>                      
-                                    </ul>
-                                </li>
-                            </ul> 
-                        </div>
-                    </div>
-                </div>                         
-            </section>
-            <!-- Info Head -->
 
             <!-- Nav-->
             <nav>
@@ -166,17 +109,11 @@
                         <!-- Menu-->
                         <ul id="menu" class="col-md-9 sf-menu">
                             <li>
-                                <a href="index.html">HOME</a>
-                                <ul> 
-                                    <li><a href="index.html">Home Version 1</a></li>                                 
-                                    <li><a href="index-list.html">Home Version 2</a></li>
-                                    <li><a href="index-map-one.html">Home Version 3</a></li>
-                                    <li><a href="index-map-two.html">Home Version 4</a></li>
-                                </ul>
+                                <a href="/">HOME</a>
                             </li>
                             <li><a href="about.html">ABOUT</a></li>
                              <li>
-                                <a href="#">FEATURES</a>
+                                <a href="#">AVAILABLE HOMES</a>
                                 <ul> 
                                     <li><a href="#">Pages</a>
                                         <ul>                                  
@@ -192,15 +129,15 @@
                                 </ul>
                             </li>  
                             <li>
-                                <a href="#">PROPERTIES</a>
+                                <a href="#">DESIGN &amp; FLOOR PLANS</a>
                                 <ul>                                  
                                     <li><a href="properties.html">Properties</a></li>
                                     <li><a href="properties_two.html">Properties Two</a></li>
                                     <li><a href="details_properties.html">Details Properties</a></li>
                                 </ul>
-                            </li>  
+                            </li> 
                             <li>
-                                <a href="#">BLOG</a>
+                                <a href="#">LOCAL AREA</a>
                                 <ul>                                  
                                     <li><a href="blog.html">Post</a></li>
                                     <li><a href="single_post.html">Single Post</a></li>                                
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -9,228 +9,7 @@ Riata Homes by Chesmar | Home
 @section('content')
 
 <!-- Header-->
-<header> 
-    <!-- Filter Search-->
-    <div class="container">
-        <div class="row">
-            <div class="col-md-5">                        
-                <div class="row bg_header">
-                    <ul class="tabs_services">
-                        <li><a id="1" class="switcher set2">Calculator</a></li>
-                        <li><a id="2" class="switcher set2">Buying</a></li>          
-                        <li><a id="3" class="switcher set2">Selling</a></li>
-                        <li><a id="4" class="switcher set2">Renting</a></li>                                    
-                    </ul>
-                    
-                    <div class="clearfix"></div>
-                    <div class="switcher-panel"></div> 
-
-                    <!-- 1-content -->
-                    <div id="1-content" class="switcher-content set2 show">
-                        <div class="search_box calculator">
-                            <form action="php/calculator.php" id="calculator">
-                              <label>Calculate Your Propertie</label>
-                                <div>                                                
-                                    <input type="number" required placeholder="Price Propertie" name="price">
-                                </div> 
-                                <div>                                                
-                                    <input type="number" required placeholder="Down payment" name="payment">
-                                </div> 
-                                <div>                                                
-                                    <select name="interest">
-                                        <option>Interest Rate</option>                                  
-                                        <option value="1.39">1.39 %</option>
-                                        <option value="2">2%</option>
-                                        <option value="3">3%</option>              
-                                    </select>
-                                </div> 
-                                <div>                                                
-                                    <select name="months">
-                                        <option>Months</option>                                  
-                                        <option>36</option>
-                                        <option>48</option>
-                                        <option>64</option>  
-                                        <option>180</option>              
-                                    </select>
-                                </div>                                                                               
-                                <div>
-                                    <input type="submit" class="button" value="Calcule">
-                                </div> 
-                            </form>   
-                            <div id="result_calculator"></div>    
-                         </div>            
-                    </div>
-                    <!-- End 1-content -->
-
-                    <!-- 2-content -->
-                    <div id="2-content" class="switcher-content set2">
-                       <div class="search_box">
-                            <form action="properties.html">
-                                <div class="">
-                                    <label>Search</label>
-                                    <input type="text" required>
-                                </div>
-                                <div>
-                                    <label>Location</label>
-                                    <select name="location">
-                                        <option value="">-- Select city --</option>
-                                         <option value="0" selected="selected">New York</option>
-                                         <option value="1">Los Angeles</option>
-                                         <option value="2">Chicago</option>
-                                         <option value="3">Miami</option>
-                                         <option value="4">San Francisco</option>
-                                         <option value="5">Boston</option>
-                                         <option value="6">Philadelphia</option>
-                                         <option value="7">Dallas</option>                    
-                                     </select>
-                                </div>
-                                <div>
-                                    <label>Price Range</label>                                     
-                                    <select name="pricing">
-                                          <option>View all</option>                                  
-                                        <option value="info_price">0 - 300.000</option>
-                                        <option value="info_price">300.000 - 500.000</option>
-                                        <option value="info_price">500.000 - 800.000</option>
-                                        <option value="info_price">800.000 - 1.000.000</option>
-                                        <option value="info_price">10.000.000 +</option>    
-                                    </select>
-                                </div>
-                                <div>
-                                    <label>Area</label>
-                                    <select name="area">
-                                        <option>View all</option>
-                                        <option value="info_area">0 - 60 m²</option>
-                                        <option value="info_area">60 m² - 90 m²</option>
-                                        <option value="info_area">90 m² - 150 m²</option>
-                                        <option value="info_area">150 m² - 240 m²</option>
-                                        <option value="info_area">240 m² - 360 m²</option>
-                                        <option value="info_area">360 m² - 480 m²</option>
-                                        <option value="info_area">480 m² - 600 m²</option>
-                                        <option value="info_area">More 600 m²</option>
-                                    </select>
-                                </div>
-                                <div>
-                                    <input type="submit" class="button" value="Search">
-                                </div> 
-                            </form>                               
-                        </div>   
-                    </div>
-                    <!-- End 1-content -->
-
-                    <!-- 3-content -->
-                    <div id="3-content" class="switcher-content set2">
-                       <div class="search_box">
-                            <form action="properties.html">
-                                <div>
-                                    <label>Search</label>
-                                    <input type="text" required>
-                                </div>
-                                <div>
-                                    <label>Location</label>
-                                    <select name="location">
-                                        <option value="">-- Select city --</option>
-                                         <option value="0" selected="selected">New York</option>
-                                         <option value="1">Los Angeles</option>
-                                         <option value="2">Chicago</option>
-                                         <option value="3">Miami</option>
-                                         <option value="4">San Francisco</option>
-                                         <option value="5">Boston</option>
-                                         <option value="6">Philadelphia</option>
-                                         <option value="7">Dallas</option>                    
-                                     </select>
-                                </div>
-                                <div>
-                                    <label>Price Range</label>                                     
-                                    <select name="pricing">
-                                          <option>View all</option>                                  
-                                        <option value="info_price">0 - 300.000</option>
-                                        <option value="info_price">300.000 - 500.000</option>
-                                        <option value="info_price">500.000 - 800.000</option>
-                                        <option value="info_price">800.000 - 1.000.000</option>
-                                        <option value="info_price">10.000.000 +</option>    
-                                    </select>
-                                </div>
-                                <div>
-                                    <label>Area</label>
-                                    <select name="area">
-                                        <option>View all</option>
-                                        <option value="info_area">0 - 60 m²</option>
-                                        <option value="info_area">60 m² - 90 m²</option>
-                                        <option value="info_area">90 m² - 150 m²</option>
-                                        <option value="info_area">150 m² - 240 m²</option>
-                                        <option value="info_area">240 m² - 360 m²</option>
-                                        <option value="info_area">360 m² - 480 m²</option>
-                                        <option value="info_area">480 m² - 600 m²</option>
-                                        <option value="info_area">More 600 m²</option>
-                                    </select>
-                                </div>
-                                <div>
-                                    <input type="submit" class="button" value="Search">
-                                </div> 
-                            </form>                               
-                        </div>   
-                    </div>
-                    <!-- End 3-content -->
-
-                    <!-- 4-content -->
-                    <div id="4-content" class="switcher-content set2">
-                       <div class="search_box">
-                            <form action="properties.html">
-                                <div>
-                                    <label>Search</label>
-                                    <input type="text" required>
-                                </div>
-                                <div>
-                                    <label>Location</label>
-                                    <select name="location">
-                                        <option value="">-- Select city --</option>
-                                         <option value="0" selected="selected">New York</option>
-                                         <option value="1">Los Angeles</option>
-                                         <option value="2">Chicago</option>
-                                         <option value="3">Miami</option>
-                                         <option value="4">San Francisco</option>
-                                         <option value="5">Boston</option>
-                                         <option value="6">Philadelphia</option>
-                                         <option value="7">Dallas</option>                    
-                                     </select>
-                                </div>
-                                <div>
-                                    <label>Price Range</label>                                     
-                                    <select name="pricing">
-                                          <option>View all</option>                                  
-                                        <option value="info_price">0 - 300.000</option>
-                                        <option value="info_price">300.000 - 500.000</option>
-                                        <option value="info_price">500.000 - 800.000</option>
-                                        <option value="info_price">800.000 - 1.000.000</option>
-                                        <option value="info_price">10.000.000 +</option>    
-                                    </select>
-                                </div>
-                                <div>
-                                    <label>Area</label>
-                                    <select name="area">
-                                        <option>View all</option>
-                                        <option value="info_area">0 - 60 m²</option>
-                                        <option value="info_area">60 m² - 90 m²</option>
-                                        <option value="info_area">90 m² - 150 m²</option>
-                                        <option value="info_area">150 m² - 240 m²</option>
-                                        <option value="info_area">240 m² - 360 m²</option>
-                                        <option value="info_area">360 m² - 480 m²</option>
-                                        <option value="info_area">480 m² - 600 m²</option>
-                                        <option value="info_area">More 600 m²</option>
-                                    </select>
-                                </div>
-                                <div>
-                                    <input type="submit" class="button" value="Search">
-                                </div> 
-                            </form>                               
-                        </div>   
-                    </div>
-                    <!-- End 4-content -->
-                </div>       
-            </div>
-        </div>
-    </div>  
-    <!-- Filter Search-->              
+<header>               
     
     <!-- Slide -->           
     <div class="camera_wrap camera_white_skin" id="slide">
@@ -825,7 +604,7 @@ Riata Homes by Chesmar | Home
                   <div class="thumbnail-process">
                     <div class="caption-head">
                       <em class="caption-icon fa fa-check icon-big"></em>                            
-                      <h2 class="caption-title">Register</h2>
+                      <h2 class="caption-title">Discover</h2>
                     </div>
                   </div>
               </div>
@@ -837,7 +616,7 @@ Riata Homes by Chesmar | Home
                   <div class="thumbnail-process">
                     <div class="caption-head">
                       <em class="caption-icon fa fa-edit icon-big"></em>
-                        <h2 class="caption-title">Publish</h2>
+                        <h2 class="caption-title">Match</h2>
                     </div>
                   </div>
               </div>
@@ -848,7 +627,7 @@ Riata Homes by Chesmar | Home
                 <div class="thumbnail-process">
                   <div class="caption-head">
                     <em class="caption-icon fa fa-credit-card icon-big"></em>
-                      <h2 class="caption-title">Monetize</h2>
+                      <h2 class="caption-title">Sign</h2>
                   </div>
                 </div>
             </div>
@@ -857,8 +636,8 @@ Riata Homes by Chesmar | Home
             <div class="col-md-3">
                 <div class="thumbnail-process">
                   <div class="caption-head">
-                    <em class="caption-icon fa fa-plane icon-big"></em>
-                      <h2 class="caption-title">Success</h2>
+                    <em class="caption-icon fa fa-home icon-big"></em>
+                      <h2 class="caption-title">Happiness</h2>
                   </div>                           
                 </div>
             </div>
