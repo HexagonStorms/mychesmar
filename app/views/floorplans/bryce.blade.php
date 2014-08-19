762f69d5851ae579ff8f6692e8759f5cef90855a
Josue Plaza <JosuePlaza@Josues-MacBook-Pro.local>
2014-08-19 18:17:24 -0500

    added descriptions of energy savings and comfort tab

--- app/views/floorplans/bryce.blade.php
+++ app/views/floorplans/bryce.blade.php
@@ -87,48 +87,30 @@ Floor Plans | Bryce
                             
                 <!--CONTAINER TABS-->   
                 <div class="tab_container"> 
-                    <!-- Tab 1 -->      
+                    <!-- Tab 1: Energy Savings -->      
                     <div id="tab1" class="tab_content">
                         <div class="row">
-                            <div class="col-md-6">
-                                <h4>General Features</h4>
-                                <div class="row">
-                                    <div class="col-xs-12 col-sm-4 col-md-4">
-                                        <ul class="general_info">
-                                            <li><i class="icon-ok"></i> Air conditioning</li>
-                                            <li><i class="icon-ok"></i> Balcony</li>
-                                            <li><i class="icon-ok"></i> Bedding</li>
-                                            <li><i class="icon-remove"></i>Cable TV</li>      
-                                            <li><i class="icon-ok"></i> Wifi</li>
-                                            <li><i class="icon-remove"></i>DVD</li>                     
-                                        </ul>
-                                    </div>
-                                    <div class="col-xs-12 col-sm-4 col-md-4">
-                                        <ul class="general_info">
-                                            <li><i class="icon-ok"></i> Wifi</li>
-                                            <li><i class="icon-remove"></i>DVD</li>
-                                            <li><i class="icon-ok"></i> Balcony</li>
-                                            <li><i class="icon-ok"></i> Bedding</li>
-                                            <li><i class="icon-ok"></i> Parking</li>
-                                            <li><i class="icon-ok"></i> Computer</li>                           
-                                        </ul>
-                                    </div>
-                                    <div class="col-xs-12 col-sm-4 col-md-4">
-                                        <ul class="general_info">
-                                            <li><i class="icon-ok"></i> Radio</li>
-                                            <li><i class="icon-remove"></i>Terrace</li>                              
-                                            <li><i class="icon-remove"></i>Use of pool</li>
-                                            <li><i class="icon-ok"></i> Microwave</li>
-                                            <li><i class="icon-ok"></i> Wifi</li>
-                                            <li><i class="icon-remove"></i>DVD</li>
-                                        </ul>
-                                    </div>
-                                </div>
+                            <div class="col-xs-12">
+                                <h4>Energy Savings &amp; Comfort</h4>
+                                <ul class="general_info">
+                                    <li>Energy Star Certified Homes, (Optional)</li>
+                                    <li>Ce3 Heating &amp; Cooling Energy Certification</li>
+                                    <li>Carrier High Efficiency 14 SEER HVAC system with Puron</li>
+                                    <li>Carrier High Efficiency gas furnace</li>
+                                    <li>R-8 HVAC ductwork with fresh air intake</li>
+                                    <li>HVAC jumper ducts for room balancing</li>
+                                    <li>Honeywell digital thermostat</li>
+                                    <li>R-13 cellulose wall insulation</li>
+                                    <li>R-30 cellulose blown-in attic insulation</li>
+                                    <li>R-50 cellulose insulation between garage ceiling and living areas</li>
+                                    <li>R-22 cellulose insulation on sloped ceilings</li>
+                                    <li>1/2” Dow R-3 insulated sheathing</li>
+                                    <li>Polycel insulation around windows, doors, bottom plate and other openings</li>
+                                    <li>Double-pane LoE3 insulated, white vinyl windows (tilt-in for easy clean)</li>
+                                    <li>Radiant Barrier Sheathing</li>
+                                </ul>
                             </div>
-                            <div class="col-md-6">
-                                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                            </div>
+                            
                         </div>    
 
                         <!-- Divisor-->
@@ -141,47 +123,16 @@ Floor Plans | Bryce
                     </div>
                     <!-- END TAB 1 -->      
 
-                    <!-- TAB 2 -->      
+                    <!-- TAB 2 Exterior-->      
                     <div id="tab2" class="tab_content">                                                             
                        <div class="row">
-                            <div class="col-md-6">
-                                <h4>General Features</h4>
-                                <div class="row">
-                                    <div class="col-xs-12 col-sm-4 col-md-4">
-                                        <ul class="general_info">
-                                            <li><i class="icon-ok"></i> Air conditioning</li>
-                                            <li><i class="icon-ok"></i> Balcony</li>
-                                            <li><i class="icon-ok"></i> Bedding</li>
-                                            <li><i class="icon-remove"></i>Cable TV</li>      
-                                            <li><i class="icon-ok"></i> Wifi</li>
-                                            <li><i class="icon-remove"></i>DVD</li>                     
-                                        </ul>
-                                    </div>
-                                    <div class="col-xs-12 col-sm-4 col-md-4">
-                                        <ul class="general_info">
-                                            <li><i class="icon-ok"></i> Wifi</li>
-                                            <li><i class="icon-remove"></i>DVD</li>
-                                            <li><i class="icon-ok"></i> Balcony</li>
-                                            <li><i class="icon-ok"></i> Bedding</li>
-                                            <li><i class="icon-ok"></i> Parking</li>
-                                            <li><i class="icon-ok"></i> Computer</li>                           
-                                        </ul>
-                                    </div>
-                                    <div class="col-xs-12 col-sm-4 col-md-4">
-                                        <ul class="general_info">
-                                            <li><i class="icon-ok"></i> Radio</li>
-                                            <li><i class="icon-remove"></i>Terrace</li>                              
-                                            <li><i class="icon-remove"></i>Use of pool</li>
-                                            <li><i class="icon-ok"></i> Microwave</li>
-                                            <li><i class="icon-ok"></i> Wifi</li>
-                                            <li><i class="icon-remove"></i>DVD</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                            <div class="col-xs-12">
+                                <h4>Exterior</h4>
+                                <ul class="general_info">
+                                    <li>Covered patio included on all homes</li>
+                                    <li>3 sides brick</li>
+                                    
+                                </ul>
                             </div>
                         </div>  
 
@@ -195,49 +146,17 @@ Floor Plans | Bryce
                     </div>
                     <!-- END TAB 2 -->
 
-                    <!-- Tab 3 -->      
+                    <!-- Tab 3 Interior -->      
                     <div id="tab3" class="tab_content">                
                         <div class="row">
-                            <div class="col-md-6">
-                                <h4>General Features</h4>
-                                <div class="row">
-                                    <div class="col-xs-12 col-sm-4 col-md-4">
-                                        <ul class="general_info">
-                                            <li><i class="icon-ok"></i> Air conditioning</li>
-                                            <li><i class="icon-ok"></i> Balcony</li>
-                                            <li><i class="icon-ok"></i> Bedding</li>
-                                            <li><i class="icon-remove"></i>Cable TV</li>      
-                                            <li><i class="icon-ok"></i> Wifi</li>
-                                            <li><i class="icon-remove"></i>DVD</li>                     
-                                        </ul>
-                                    </div>
-                                    <div class="col-xs-12 col-sm-4 col-md-4">
-                                        <ul class="general_info">
-                                            <li><i class="icon-ok"></i> Wifi</li>
-                                            <li><i class="icon-remove"></i>DVD</li>
-                                            <li><i class="icon-ok"></i> Balcony</li>
-                                            <li><i class="icon-ok"></i> Bedding</li>
-                                            <li><i class="icon-ok"></i> Parking</li>
-                                            <li><i class="icon-ok"></i> Computer</li>                           
-                                        </ul>
-                                    </div>
-                                    <div class="col-xs-12 col-sm-4 col-md-4">
-                                        <ul class="general_info">
-                                            <li><i class="icon-ok"></i> Radio</li>
-                                            <li><i class="icon-remove"></i>Terrace</li>                              
-                                            <li><i class="icon-remove"></i>Use of pool</li>
-                                            <li><i class="icon-ok"></i> Microwave</li>
-                                            <li><i class="icon-ok"></i> Wifi</li>
-                                            <li><i class="icon-remove"></i>DVD</li>
-                                        </ul>
-                                    </div>
-                                </div>
+                            <div class="col-xs-12">
+                                <h4>Exterior</h4>
+                                <ul class="general_info">
+                                    <li></li>
+                                    
+                                </ul>
                             </div>
-                            <div class="col-md-6">
-                                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                            </div>
-                        </div>
+                        </div> 
 
                         <!-- Divisor-->
                         <div class="divisor">
@@ -253,6 +172,22 @@ Floor Plans | Bryce
                     <div id="tab4" class="tab_content">                
                         <div class="row">
                             <div class="col-md-6">
+                                <h4>Master Bath</h4>
+                                <div class="col-xs-12">
+                                    <ul class="general_info">
+                                        <li>Royal Granite cultured marble vanity tops with under-mount sink</li>
+                                        <li>36” high vanity cabinet with double sink and drawers per plan</li>
+                                        <li>Moen chrome washer-less faucets</li>
+                                        <li>Medicine cabinet</li>      
+                                        <li>Garden tub with ceramic tile backsplash</li>
+                                        <li>Separate shower with ceramic tile</li>
+                                        <li>Walk-in closet with generous shelving</li>
+                                        </li>Elongated commode</li>                     
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
                                 <h4>Kitchen</h4>
                                 <div class="col-xs-12">
                                     <ul class="general_info">
@@ -267,46 +202,32 @@ Floor Plans | Bryce
                                 </div>
                             </div>
 
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h4>Secondary Bathroom</h4>
+                                <div class="col-xs-12">
+                                    <ul class="general_info">
+                                        <li>Royal Granite cultured marble vanity tops with under-mount sink</li>
+                                        <li>Moen chrome washer-less faucets</li>
+                                        <li>Medicine cabinet</li>
+                                        <li>Tub with ceramic tile shower walls</li>      
+                                        <li>Elongated commode</li>                     
+                                    </ul>
+                                </div>
+                            </div>
+
                             <div class="col-md-6">
                                 <h4>Laundry Room</h4>
                                 <div class="col-xs-12">
                                     <ul class="general_info">
                                         <li>Overhead storage shelf with clothing rod</li>
-                                        <li>Storage and/or freezer space per plan</li>
-                                                             
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-md-6">
-                                <h4>Master Bathroom</h4>
-                                <div class="col-xs-12">
-                                    <ul class="general_info">
-                                        <li><i class="icon-ok"></i> Air conditioning</li>
-                                        <li><i class="icon-ok"></i> Balcony</li>
-                                        <li><i class="icon-ok"></i> Bedding</li>
-                                        <li><i class="icon-remove"></i>Cable TV</li>      
-                                        <li><i class="icon-ok"></i> Wifi</li>
-                                        <li><i class="icon-remove"></i>DVD</li>                     
+                                        <li>Storage and/or freezer space per plan</li>                     
                                     </ul>
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
-                                <h4>Secondary Bathroom</h4>
-                                <div class="col-xs-12">
-                                    <ul class="general_info">
-                                        <li><i class="icon-ok"></i> Air conditioning</li>
-                                        <li><i class="icon-ok"></i> Balcony</li>
-                                        <li><i class="icon-ok"></i> Bedding</li>
-                                        <li><i class="icon-remove"></i>Cable TV</li>      
-                                        <li><i class="icon-ok"></i> Wifi</li>
-                                        <li><i class="icon-remove"></i>DVD</li>                     
-                                    </ul>
-                                </div>
-                            </div>
                         </div>
 
                         <!-- Divisor-->
