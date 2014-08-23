2b902f0eff2834d963d3bef96e3a79042baeeaba
Josue Plaza <josueplazamusic@gmail.com>
2014-08-23 12:53:45 -0500

    Added floorplan template

---
+++ app/views/floorplans/floorplantemplate.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.floorplans')
+@section('floorplanname') @stop
+@section('floorplanpdf')  @stop
+@section('floorplanelevation')  @stop
+
+@section('wowslider')
+<div class="ws_images">
+    <ul>
+        <li><img src="/img/property/_759x500.png" alt="house photos" title="house photos" id="wows1_0"/></li>
+        <li><img src="/img/property/_759x500.png" alt="house photos" title="house photos" id="wows1_1"/></li>
+        <li><img src="/img/property/_759x500.png" alt="house photos" title="house photos" id="wows1_2"/></li>
+        <li><img src="/img/property/_759x500.png" alt="house photos" title="house photos" id="wows1_3"/></li>
+        <li><img src="/img/property/_759x500.png" alt="house photos" title="house photos" id="wows1_4"/></li>
+        <li><img src="/img/property/_759x500.png" alt="house photos" title="house photos" id="wows1_5"/></li>
+        <li><img src="/img/property/_759x500.png" alt="house photos" title="house photos" id="wows1_6"/></li>
+        <li><img src="/img/property/_759x500.png" alt="house photos" title="house photos" id="wows1_7"/></li>
+    </ul>
+</div>
+<div class="ws_thumbs">
+    <div>
+        <a href="#" title="house photos"><img src="/img/property/_759x500.png" alt="house photos" /></a>
+        <a href="#" title="house photos"><img src="/img/property/_759x500.png" alt="house photos" /></a>
+        <a href="#" title="house photos"><img src="/img/property/_759x500.png" alt="house photos" /></a>
+        <a href="#" title="house photos"><img src="/img/property/_759x500.png" alt="house photos" /></a>
+        <a href="#" title="house photos"><img src="/img/property/_759x500.png" alt="house photos" /></a>
+        <a href="#" title="house photos"><img src="/img/property/_759x500.png" alt="house photos" /></a>
+        <a href="#" title="house photos"><img src="/img/property/_759x500.png" alt="house photos" /></a>
+        <a href="#" title="house photos"><img src="/img/property/_759x500.png" alt="house photos" /></a>
+    </div>
+</div>
+@stop
+
+@section('description')
+
+@stop
+
+@section('price')  @stop
+@section('sqft')  @stop
+@section('story')  @stop
+@section('bath')  @stop
+@section('bed')  @stop
+@section('tour') <a href=""></a> @stop
