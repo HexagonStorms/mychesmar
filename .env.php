49e52412bb2fe44d25890e7a0c93c9827929ba81
Josue Plaza <josueplazamusic@gmail.com>
2014-09-01 12:58:33 -0500

    added env for site protection

---
+++ .env.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+    'DB_NAME' => 'mychesmar',
+    'DB_USER' => 'codeup',
+	'DB_PASS' => 'letmein',
+
+	'ADMIN_USER' => 'admin@codeup.com',
+	'ADMIN_PASS' => 'adminPass123!',
+
+);
